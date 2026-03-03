de761aea67bb491b8ab22699979fddd7626ba711
Yordin Figueroa <yordinfigueroa2006@gmail.com>
2026-03-02 20:39:57 -0500

    subiendo todos los archivos

---
+++ listas-simples/sistema-contactos/src/Nodo.java
@@ -0,0 +1,24 @@
+public class Nodo {
+    private Contacto dato;
+    private Nodo siguiente;
+
+    public Contacto getDato() {
+        return dato;
+    }
+
+    public void setDato(Contacto dato) {
+        this.dato = dato;
+    }
+
+    public Nodo getSiguiente() {
+        return siguiente;
+    }
+
+    public void setSiguiente(Nodo siguiente) {
+        this.siguiente = siguiente;
+    }
+
+    public Nodo(Contacto dato) {
+        this.dato = dato;
+    }
+}
